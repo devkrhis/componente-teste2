e311af4d8495334dac3cf231e347516171d76ea6
krhistopher <krhistopher@weon.com.br>
2022-11-08 22:43:33 -0300

    aula 110 finalizada

---
+++ src/components/NewContadorGlobal.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="oldContador">
+        <testeNewContador v-for="c in 5" :key="c.id"/>
+        
+    </div>
+    
+</template>
+
+<script>
+import NewContador from './NewContador.vue' // aqui eu estou importando.
+
+export default {
+    components: { 'testeNewContador': NewContador} // // nome que eu quero referenciar apontado para qual componente eu quero localmente.
+
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-import NewContador from './components/NewContador.vue'
+import NewContadorGlobal from './components/NewContadorGlobal.vue'
 
 Vue.config.productionTip = false
-Vue.component('testeContador', NewContador) // nome que eu quero referenciar apontado para qual componente eu quero
+Vue.component('testeContadorGlobal', NewContadorGlobal) // nome que eu quero referenciar apontado para qual componente eu quero
 
 
 new Vue({
